b4b1fc0b68d437679e1cccec5d339393444cafc7
dulmaranspace4 <serulmrls@hotmail.com>
2025-09-01 08:38:31 -0700

    Updated mongoose connection options to include useCreateIndex.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('Connecté à MongoDB'))
   .catch(err => console.error('Erreur de connexion à MongoDB', err));
 
